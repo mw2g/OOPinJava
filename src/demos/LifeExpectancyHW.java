4a104c8c1018b28a270505d063ab0dad23695c30
mw2g@ya.ru <nikulmiH1356>
2019-11-28 21:10:28 +0700

    5th week

---
+++ src/demos/LifeExpectancyHW.java
@@ -0,0 +1,73 @@
+package demos;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import de.fhpotsdam.unfolding.UnfoldingMap;
+import de.fhpotsdam.unfolding.data.Feature;
+import de.fhpotsdam.unfolding.data.GeoJSONReader;
+import de.fhpotsdam.unfolding.marker.Marker;
+import de.fhpotsdam.unfolding.providers.Google;
+import de.fhpotsdam.unfolding.utils.MapUtils;
+import processing.core.PApplet;
+
+public class LifeExpectancyHW extends PApplet {
+	private UnfoldingMap map;
+	private Map<String, Float> lifeExpByCountry;
+	List<Feature> countries = new ArrayList<>();
+	List<Marker> countryMarkers;
+	
+	@Override
+	public void setup() {
+		size(800, 600, OPENGL);
+		map = new UnfoldingMap(this, 50, 50, 700, 500, new Google.GoogleMapProvider());
+		MapUtils.createDefaultEventDispatcher(this, map);
+		
+		lifeExpByCountry = loadLifeExpactancyFromCSV("LifeExpectancyWorldBankModule3.csv");
+		
+		countries = GeoJSONReader.loadData(this, "countries.geo.json");
+		countryMarkers = MapUtils.createSimpleMarkers(countries);
+		
+		map.addMarkers(countryMarkers);
+		shadeCountries();
+	}
+
+	private void shadeCountries() {
+		for (Marker marker : countryMarkers) {
+			String countryId = marker.getId();
+			
+			if (lifeExpByCountry.containsKey(countryId)) {
+				float lifeExp = lifeExpByCountry.get(countryId);
+				int colorLevel = (int) map(lifeExp, 40, 90, 10, 255);
+				marker.setColor(color(255-colorLevel, 100, colorLevel));
+			}
+			else {
+				marker.setColor(color(150, 150, 150));
+			}
+		}
+	}
+
+	@Override
+	public void draw() {
+		map.draw();
+	}
+	
+	private Map<String, Float> loadLifeExpactancyFromCSV(String fileName){
+		Map<String, Float> lifeExpMap = new HashMap<String, Float>();
+		String[] rows = loadStrings(fileName);
+		
+		for (String row : rows) {
+			String[] columns = row.split(",");
+			if (columns.length == 6 && !columns[5].equals("..")) {
+				float value = Float.parseFloat(columns[5]);
+				lifeExpMap.put(columns[4], value);
+			}
+		}
+		
+		return lifeExpMap;
+	}
+	
+
+}
---
+++ src/demos/MyDisplay.java
@@ -0,0 +1,41 @@
+package demos;
+
+import processing.core.PApplet;
+
+/** 
+ * A class to illustrate some use of the PApplet class
+ * Used in module 3 of the UC San Diego MOOC Object Oriented Programming in Java
+ * 
+ * @author UC San Diego Intermediate Software Development MOOC team
+ * 
+ *
+ */
+public class MyDisplay extends PApplet {
+	double x = 0;
+	
+	public void setup()
+	{
+		size(400, 400);
+		background(200, 200, 200);
+		
+	}
+	
+	public void draw()
+	{
+		fill(255, 255, 0);
+		ellipse(200, 200, 390, 390);
+		fill(0, 0, 0);
+		ellipse(120, 130, 50, 70);
+		x = x + 10;
+		if (x > width) {
+			x = 0;
+		}else {
+			ellipse(280, 130, 50, 70);
+		}
+		
+		
+		noFill();
+		arc(200, 280, 75, 75, 0, PI);
+	}
+	
+}
